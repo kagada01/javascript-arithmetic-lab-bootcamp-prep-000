a579ceb77d8239b83b5c64f6a9faad68e83b4b83
Dan Kagan <dankagan7@gmail.com>
2019-02-11 02:29:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,31 +3,21 @@ function add(a, b){
 }
 
 function subtract(a, b){
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
   return(a-b)
 }
 
 function multiply(a, b){
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
   return(a*b)
 }
 
 function divide(a, b){
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
   return(a/b)
 }
 
 function inc(a){
-  a = Math.floor(Math.random() * 1000)
-  a++
   return(a)
 }
 
 function dec(a){
-  a = Math.floor(Math.random() * 1000)
-  a--
   return(a)
 }
