4c312b5b9915f4d3df35961af57636f6fb9ebb28
Dan Kagan <dankagan7@gmail.com>
2019-02-11 02:19:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function add(a, b){
   b = Math.floor(Math.random() * 1000)
   return(a+b)
 }
+
+function subtract(a, b){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+  return(a-b)
+}
